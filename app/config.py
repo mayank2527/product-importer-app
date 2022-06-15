f3cc568d589f92bb583dce0d50857cf864e2fa69
Mayank Chhabra <mayankchhabra2527@gmail.com>
2022-06-15 07:10:15 +0530

    added postgres,celery,redis and dockerize the app

--- app/config.py
+++ app/config.py
@@ -12,4 +12,17 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 
-config_settings = {"development": DevelopmentConfig}
+class CeleryConfig:
+    CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+
+
+class FileUploadConfig:
+    FILE_UPLOAD_PATH = os.environ.get("FILE_UPLOAD_PATH")
+
+
+config_settings = {
+    "development": DevelopmentConfig,
+    "celery_config": CeleryConfig,
+    "file_upload_config": FileUploadConfig,
+}
